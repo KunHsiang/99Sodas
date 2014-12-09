b95fbeadede5d37ce2b9d9f6e5a64eaef9528e16
ChangKH <web@kunhsiang.com>
2014-12-09 15:33:18 +0800

    First setup!

---
+++ section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+for var bottlesOfSoda = 99; bottlesOfSoda >= 0; --bottlesOfSoda {
+    if bottlesOfSoda == 0 {
+        println("No more bottles of soda on the wall")
+    } else {
+        println("\(bottlesOfSoda) bottles of soda on the wall")
+    }
+}
